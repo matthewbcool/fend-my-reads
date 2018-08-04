081e808f71e282ab75629872d4963761b86e4bdf
Matt Cool <matthewbcool@gmail.com>
2018-08-04 17:09:40 -0500

    added title prop to bookshelf and imported react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBar from './Components/SearchBar'
 import BookShelf from './Components/BookShelf'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -10,38 +15,31 @@ class BooksApp extends React.Component {
      * TODO: import react-router-dom and use <Route> and <Link> components to adjust URL on clicks and display the right component. 
      * https://reacttraining.com/react-router/web/example/basic
      */
-    showSearchPage: false,
   }
-
-
-      /**
-     * TODO: decomposition of this whole thing....
-     */
-
   render() {
     
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBar />
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf />
-                <BookShelf />
-                <BookShelf />
+      <Router>
+        <div className="app">
+            <SearchBar />
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <BookShelf title="Currently Reading" />
+                  <BookShelf title="Want to Read" />
+                  <BookShelf title="Read" />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search"><a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a> </Link>
               </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
-      </div>
+            
+        </div>
+      </Router>
     )
   }
 }
