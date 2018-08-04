d7ebe0eba5147573787753bd4634434435a4a01a
Matt Cool <matthewbcool@gmail.com>
2018-08-04 16:24:53 -0500

    reduced static pieces into components

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../App.css'
+
+class SearchBar extends React.Component {
+    state = {
+        query: ""
+    }
+    updateSearch = (query) => {
+        this.setState({ query: query })
+      }
+
+    render() {
+        const { query } = this.state
+        return(
+            <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                 <input 
+                    type="text" 
+                    placeholder="Search by titles or author"
+                    value={ query }
+                    onChange={(event) => {this.updateSearch(event.target.value)} } />
+              </div>
+              <div>TEST: {JSON.stringify(this.state)}</div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default SearchBar
