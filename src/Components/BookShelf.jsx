59db32de0c651fbfd089209a5e271a63f0b2be7d
Matt Cool <matthewbcool@gmail.com>
2018-08-07 22:56:08 -0500

    removed console logs

--- src/Components/BookShelf.jsx
+++ src/Components/BookShelf.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import '../App.css'
-import Book from './Book'
+//import Book from './Book'
 
 // TO DO: render JSON info to Books
 class BookShelf extends React.Component {
     render() {
-        console.log(this.props.wantToRead, this.props.currentlyReading, this.props.read)
         return(
             <div className="bookshelf">
             <h2 className="bookshelf-title">{this.props.title}</h2>
