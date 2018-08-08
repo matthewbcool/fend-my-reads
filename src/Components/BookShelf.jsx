4760fe2d4d15d15320cdf7ea42b5dcbfa56c1432
Matt Cool <matthewbcool@gmail.com>
2018-08-08 07:33:48 -0500

    migrated filter code to bookshlef

--- src/Components/BookShelf.jsx
+++ src/Components/BookShelf.jsx
@@ -5,13 +5,25 @@ import Book from './Book'
 // TO DO: render JSON info to Books
 class BookShelf extends React.Component {
     render() {
+
+        if (this.props.bookList !== 'undefined') {
+
+        this.props.bookList.filter(book => book.shelf === 'wantToRead').map(book => wantToRead.push(book))
+        this.props.bookList.filter(book => book.shelf === 'read').map(book => read.push(book))
+        } else {console.log('whaaaa??')}
         console.log(this.props.currentlyReading, this.props.wantToRead, this.props.read)
         return(
             <div className="bookshelf">
             <h2 className="bookshelf-title">{this.props.title}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
+                  {this.props.bookList.filter(book => book.shelf === 'currentlyReading').map(book => {
+                      <li key= {book.id} >
+                        <Book book = {book} />
+                      </li>
+                  } )
                   
+                }
                   {JSON.stringify(this.props.wantToRead)}
                   {JSON.stringify(this.props.read)}
               </ol>
