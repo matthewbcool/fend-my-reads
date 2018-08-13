2012ab2561b77ba1a0d3e5ccc8cfbaf8ef70c6ea
Matt Cool <matthewbcool@gmail.com>
2018-08-13 14:01:58 -0500

    added error catching to search

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -7,10 +7,13 @@ import '../App.css'
 class Book extends React.Component {
     render() {
         let bookCover = 'url(' + this.props.book.imageLinks.thumbnail + ')'
+        let backUpCover = "url('https://mdn.mozillademos.org/files/7693/catfront.png')"
+        let decideCover = () => {return (this.props.book.imageLinks.thumbnail !== 'undefined') ? bookCover : backUpCover}
+
         return(
                 <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: bookCover }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: decideCover() }}></div>
                     <div className="book-shelf-changer">
                     <select>
                         <option value="move" disabled>Move to...</option>
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as BooksAPI from '../BooksAPI'
 import '../App.css'
 import { Link } from 'react-router-dom'
-import  Book from '../Components/Book'
+import Book from '../Components/Book'
 
 class SearchBar extends React.Component {
     constructor(props) {
@@ -17,61 +17,68 @@ class SearchBar extends React.Component {
                 publishedDate: '',
                 id: '',
                 shelf: '',
-                imageLinks: {thumbnail: ''},
-              }],
-              query: '',
-              event: ''
+                imageLinks: { thumbnail: '' },
+            }],
+            query: '',
+            event: ''
         }
         this.handleChange = this.handleChange.bind(this);
-      }
+    }
     handleChange(event) {
-    this.setState({value: event.target.value});
+        this.setState({ value: event.target.value });
     }
 
-    
+
     updateSearch = (query) => {
         //needed a way to not make the call so that I wouldn't return an undefined object
         if (query.length < 2) {
-            this.setState({query: query})
+            this.setState({ query: query })
         } else {
-    this.setState({ query: query })
-    BooksAPI.search(this.state.query).then((query) => {
-        this.setState( {queryObject: query} ) 
-    })
- }
+            this.setState({ query: query })
+            BooksAPI.search(this.state.query).then((query) => {
+                //check to see if we are throwing an error -- namley .map() is not a function --resets to empty array so we can still call .map
+                if (query.error) {
+                    this.setState({ queryObject: [] })
+                } else {
+                    this.setState({ queryObject: query })
+                }
+            })
+        }
     }
 
 
 
     render() {
-        return(
+        return (
             <div className="search-books">
-            <div className="search-books-bar">
-             <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                {}
-                 <input 
-                    type="text" 
-                    placeholder="Search by titles or author"
-                    value={ this.state.query }
-                    onChange={(event) => {
-                        this.updateSearch(event.target.value)
-                        } } />
-              </div>
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        {}
+                        <input
+                            type="text"
+                            placeholder="Search by titles or author"
+                            value={this.state.query}
+                            onChange={(event) => {
+                                this.updateSearch(event.target.value)
+                            }} />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {this.state.queryObject.map(book => {
+                            if (this.state.queryObject[0].title === "") {
+                                return <li key={book.id}>
+                                    'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'
+                                </li>
+                            } else {
+                                console.log(book)
+                                return <li key={book.id} > <Book book={book} /> </li>
+                            }
+                        })}
+                    </ol>
+                </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-              {this.state.queryObject.map(book => {
-                  if (this.state.queryObject[0].title === "" ) {
-                      return <li key={book.id}>books will appear here</li>
-                  } else {
-                        return <li key={book.id} > <Book book={book} /> </li>
-                  }
-                    })
-                }
-              </ol>
-            </div>
-          </div>
         )
     }
 }
